9926b40546d0842583d3dcd3b85f31f4e14323af
kirti81verma <106797704+kirti81verma@users.noreply.github.com>
2022-06-23 17:40:56 +0530

    Delete tutorial-series-code-chapter-4 directory

--- tutorial-series-code-chapter-4/src/__init__.py
+++
@@ -1,8 +0,0 @@
-from .config import configs
-from .utils import db, BaseMixin, ReprMixin, ma, BaseSchema, create_app, api
-
-from .blog import models
-from .users import models
-from .blog import schemas, views
-from .users import schemas, views
-
--- tutorial-series-code-chapter-4/src/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .factory import create_app
-from .api import api
-from .models import db, ReprMixin, BaseMixin
-from .schemas import ma, BaseSchema
